8d299fdfe544f9efe2bfd83a1dacaf17a7759c62
Vincent Roy <vincent.roy@emc.com>
2016-03-12 13:30:00 -0400

    Add os x defaults script.

---
+++ extras/os_x_defaults.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Based on https://github.com/henrik/dotfiles/blob/master/extras/os_x_defaults.sh
+
+echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)."
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo "Disable menu bar transparency."
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
+
+echo "Expand save panel by default."
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+echo "Disable the 'Are you sure you want to open this application?' dialog."
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+echo "Disable window animations."
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+# http://bengold.tv/post/21711266050
+echo "Speed up dock show/hide animation."
+defaults write com.apple.dock autohide-time-modifier -float 0.1
+killall Dock
+
+echo "Enable snap-to-grid for desktop icons."
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+echo "Automatically open a new Finder window when a volume is mounted."
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+
+echo "Display full POSIX path as Finder window title."
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo "Show Path bar in Finder."
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status bar in Finder."
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Use current directory as default search scope in Finder."
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo "Show the ~/Library folder."
+chflags nohidden ~/Library
+
+echo "Changed defaults. Restarting apps…"
+
+echo "Restart Finder."
+killall Finder
+
+echo "All done."
